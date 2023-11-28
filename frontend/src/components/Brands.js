1214ce254577b25f167e3c2c782bc49b834c9d48
Killian Mazibuko <mzbkil001@gmail.com>
2023-11-28 21:27:05 +0200

    Added code for using useReducer for state maanagement

--- frontend/src/components/Brands.js
+++ frontend/src/components/Brands.js
@@ -1,25 +1,54 @@
 import './Brands.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, brands: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function Brands() {
-  const [brands, setBrands] = useState([]);
+  const [{ loading, error, brands }, dispatch] = useReducer(logger(reducer), {
+    brands: [],
+    loading: true,
+    error: '',
+  });
   useEffect(() => {
-    async function getBrands() {
-      const resp = await axios.get('http://localhost:5000/api/brands');
-      setBrands(resp.data);
-    }
-    getBrands();
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/brands');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
   }, []);
   return (
     <div>
       <h1>Trusted by leading brands</h1>
       <div className="brands">
-        {brands.map((brand) => (
-          <div className="brand" key={brand.name}>
-            <img src={brand.image} alt={brand.name} />
-          </div>
-        ))}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          brands.map((brand) => (
+            <div className="brand" key={brand.name}>
+              <img src={brand.image} alt={brand.name} />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
