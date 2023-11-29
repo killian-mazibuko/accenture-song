4f52186b75c29011d62b5288f06c884da1b1eb14
Killian Mazibuko <mzbkil001@gmail.com>
2023-11-29 23:31:36 +0200

    Added build commands

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -21,6 +21,11 @@ app.use(cors());
 app.use('/api/seed', seedRouter);
 app.use('/api/brands', brandRouter);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(` Serve at http://localhost:${port}`);
